d470fcb86425c85a24ba38ea128e39a5420fadcb
Marcel Medina <marcel.net@gmail.com>
2024-08-28 04:21:41 +1200

    producer-updates

---
+++ Producer/Common/Message.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace Producer.Common
+{
+    public class Message
+    {
+        public int? Offset { get; set; }
+        public int? Partition { get; set; }
+        public string? Topic { get; set; }
+        public string? Timestamp { get; set; }
+        public string? Key { get; set; }
+        public string? Value { get; set; }
+        public List<Header>? Headers { get; set; }
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.None);
+        }
+    }
+
+    public class Header
+    {
+        public string? Key { get; set; }
+        public string? Value { get; set; }
+    }
+}
---
+++ Producer/MultipleOutputType.cs
@@ -0,0 +1,64 @@
+﻿using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Producer.Common;
+
+namespace Producer
+{
+    public class MultipleOutputType
+    {
+        [KafkaOutput("%BootstrapServers%",
+            "%Topic%",
+            Username = "%SaslUsername%",
+            Password = "%SaslPassword%",
+            Protocol = BrokerProtocol.SaslSsl,
+            EnableIdempotence = true,
+            AuthenticationMode = BrokerAuthenticationMode.Plain
+        )]
+        public string? Kevent { get; set; }
+
+        public HttpResponseData HttpResponse { get; set; }
+
+
+
+        [Function(nameof(Output))]
+
+        public static MultipleOutputType Output(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            var log = executionContext.GetLogger("HttpFunction");
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var message = req.FunctionContext
+                .BindingContext
+                .BindingData["message"]?
+                .ToString();
+
+            // Reference: https://github.com/Azure/azure-functions-kafka-extension/blob/dev/samples/dotnet-isolated/confluent/KafkaOutputWithHeaders.cs
+
+            string kevent = new Message()
+            {
+                Offset = 1, // required field, value ignored though
+                Partition = 1, // required field, value ignored though
+                Topic = "topic", // required field, value ignored though
+                Timestamp = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fffZ"),
+                Key = "1",
+                Value = message,
+                Headers = new List<Header>()
+                {
+                    new() { Key = "sample1", Value = "test1" },
+                    new() { Key = "sample2", Value = "test2" },
+                }
+            }.ToJson();
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            return new MultipleOutputType()
+            {
+                Kevent = kevent,
+                HttpResponse = response
+            };
+        }
+    }
+}
