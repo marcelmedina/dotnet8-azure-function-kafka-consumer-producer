373a68c5b511bcf5226b842bd112764828a0d756
Marcel Medina <marcel.net@gmail.com>
2024-10-08 17:51:55 +1300

    consumer-producer-updates

---
+++ Consumer/BatchProducer.cs
@@ -0,0 +1,61 @@
+﻿using Consumer.Common;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Consumer
+{
+    public class MultipleOutputTypeForBatch
+    {
+        [KafkaOutput("%BootstrapServers%",
+            "%TopicOutput%",
+            Username = "%SaslUsername%",
+            Password = "%SaslPassword%",
+            Protocol = BrokerProtocol.SaslSsl,
+            EnableIdempotence = true,
+            AuthenticationMode = BrokerAuthenticationMode.Plain
+        )]
+        public string[] Kevents { get; set; }
+    }
+
+    public class BatchProducer
+    {
+        [Function(nameof(RunBatchProducer))]
+        public static MultipleOutputTypeForBatch? RunBatchProducer(
+            [KafkaTrigger("%BootstrapServers%",
+                    "%Topic%",
+                    Username = "%SaslUsername%",
+                    Password = "%SaslPassword%",
+                    Protocol = BrokerProtocol.SaslSsl,
+                    AuthenticationMode = BrokerAuthenticationMode.Plain,
+                    IsBatched = true,
+                    ConsumerGroup = "$Default")] string[] events, FunctionContext context)
+        {
+            var logger = context.GetLogger("KafkaFunction");
+
+            try
+            {
+                var eventWithHeaders = new List<MessageWithHeader>();
+
+                foreach (var kevent in events)
+                {
+                    var message = kevent.GetMessageWithHeader();
+
+                    logger.LogInformation($"**** Message: {message.Value}");
+
+                    eventWithHeaders.Add(message);
+                }
+
+                return new MultipleOutputTypeForBatch()
+                {
+                    Kevents = eventWithHeaders.Select(e => e.ToJson()).ToArray() // Fix the ToJson call
+                };
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, ex.Message);
+            }
+
+            return null;
+        }
+    }
+}
--- Consumer/Common/Helper.cs
+++ Consumer/Common/Helper.cs
@@ -14,5 +14,22 @@ namespace Consumer.Common
                 Partition = JObject.Parse(message)["Partition"]?.ToString()
             };
         }
+
+        public static MessageWithHeader GetMessageWithHeader(this string message)
+        {
+            var parsedMessage = JObject.Parse(message);
+            var headers = parsedMessage["Headers"]?.ToObject<List<Header>>() ?? [];
+
+            return new MessageWithHeader()
+            {
+                Key = parsedMessage["Key"]?.ToString(),
+                Value = parsedMessage["Value"]?.ToString(),
+                Timestamp = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fffZ"),
+                Topic = "topic", // required field, value ignored though
+                Offset = 1,      // required field, value ignored though
+                Partition = 1,   // required field, value ignored though
+                Headers = headers
+            };
+        }
     }
 }
